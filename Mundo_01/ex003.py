57307d83cab1d22f48df931c39e82c2679c93457
Lucas Polski <lucaspolski2011@hotmail.com>
2026-02-22 13:08:16 -0300

    feat: Adicionando cores no script

--- Mundo_01/ex003.py
+++ Mundo_01/ex003.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 soma = n1 + n2
-print(f'A soma entre {n1} e {n2} é {soma}')
+print(f'\033[35mA soma entre \033[31m{n1} \033[37me \033[31m{n2} \033[37mé \033[36m{soma}\033[m')
